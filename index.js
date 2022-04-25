e7b7bf21caa3ba0e26cecfad71935096403e5279
mgalvez <mgalvez@frontsteps.com>
2022-04-25 17:11:40 -0400

    setting port 3000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import usersRoutes from "./routes/users.js";
 
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
